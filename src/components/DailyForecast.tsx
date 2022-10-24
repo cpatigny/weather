2cc9953f6c6779462ec3f58970c16c7c5d6338aa
cpatigny <63152989+cpatigny@users.noreply.github.com>
2022-10-24 14:33:07 +0200

    daily forecast styles

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -8,6 +8,7 @@ interface DailyForecastProps {
 
 const DailyForecast = ({ weather }: DailyForecastProps) => (
   <div className='daily'>
+    <p>Prévisions météo pour les 7 prochains jours</p>
     {/* the first element is the current weather and it's already displayed */}
     { weather && weather.daily.slice(1).map((dayWeather, index) => (
       <DayForecast key={index} weather={dayWeather} />
